b57c3aef49e2b88d44155deb6dd1fb35b436a025
alexanderjakobi <alexander.jakobi@outlook.com>
2024-12-20 16:26:52 +0100

    Preparation for Vercel

--- github-portfolio/next.config.mjs
+++ github-portfolio/next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  output: 'export', // Aktiviert den statischen Export
-  basePath: '/portfolio', // My GitHub-Repository
-  trailingSlash: true, // Fügt einen Slash am Ende von URLs hinzu, um GitHub Pages kompatibel zu machen
+  reactStrictMode: true, // Aktiviert Strict Mode für React
 };
 
 export default nextConfig;
