0a7fb386d6c9a00ac5f531ba33747f5ae1361d5a
alexanderjakobi <alexander.jakobi@outlook.com>
2024-12-20 16:05:59 +0100

    third commit

--- github-portfolio/next.config.mjs
+++ github-portfolio/next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export', // Aktiviert den statischen Export
+  basePath: '/portfolio', // My GitHub-Repository
+  trailingSlash: true, // Fügt einen Slash am Ende von URLs hinzu, um GitHub Pages kompatibel zu machen
 };
 
 export default nextConfig;
