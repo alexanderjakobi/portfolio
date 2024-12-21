b8a70008d896f64944407bfde29b94c12592a91c
alexanderjakobi <alexander.jakobi@outlook.com>
2024-12-21 12:00:19 +0100

    Korrektur Div

--- github-portfolio/pages/index.js
+++ github-portfolio/pages/index.js
@@ -26,10 +26,10 @@ export function IconBar(){
 
 export default function Home() {
   return (
-    <div className="bg-black text-white min-h-screen">
+    <div className="bg-black text-white min-h-screen flex flex-col lg:flex-row items-center lg:items-start p-8">
       {/* IconBar hinzufügen */}
       <IconBar />
-
+      
       {/* Bildbereich */}
       <div className="flex flex-col items-center lg:w-1/3 w-full mb-8 lg:mb-0">
         <Image
